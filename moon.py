9f4a1a6745ea6a56cc06aa74444dc20662d2a297
Niko <94224292+nokoniko@users.noreply.github.com>
2025-01-16 17:51:50 +0100

    Update moon.py

--- moon.py
+++ moon.py
@@ -1,9 +1,8 @@
-# you are free to edit this how ever you want and take the code but pls give me credit if you do!
-
-import time, requests, os, sys
+import time, requests, os, sys, base64
 from pystyle import Colorate, Colors
 from colorama import Fore
 
+successful_commands = 0 
 
 def set_window_title(title):
     if os.name == 'nt':
@@ -20,11 +19,7 @@ def set_window_size(columns, rows):
         sys.stdout.write(f"\033[8;{rows};{columns}t")
         sys.stdout.flush()
 
-
 set_window_size(90, 40)
-set_window_title("MOON SPAMMER - BY NOKONIKO ON GITHUB")
-
-os.system("cls" if os.name == "nt" else "clear")
 
 def get_webhook_name(webhook_url):
     response = requests.get(webhook_url)
@@ -35,29 +30,56 @@ def get_webhook_name(webhook_url):
         return None
 
 def update_webhook_name(webhook_url, new_name):
+    global successful_commands
     payload = {"name": new_name}
     headers = {"Content-Type": "application/json"}
     response = requests.patch(webhook_url, json=payload, headers=headers)
-
     if response.status_code == 200:
-        print(f"{Fore.GREEN}[!]{Fore.WHITE} webhook name updated!")
+        successful_commands += 1
+        print(f"{Fore.GREEN}[!]{Fore.WHITE} Webhook name updated!")
     else:
-        print(f"{Fore.RED}[!]{Fore.WHITE} something went wrog: {response.status_code} - {response.text}")
+        print(f"{Fore.RED}[!]{Fore.WHITE} Something went wrong: {response.status_code} - {response.text}")
 
-def update_webhook_profile(webhook_url, new_avatar_url):
-    payload = {
-        "avatar": new_avatar_url 
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    
-    response = requests.patch(webhook_url, json=payload, headers=headers)
-    
-    if response.status_code == 200:
-        print(f"{Fore.GREEN}[!]{Fore.WHITE} webhook pfp updated!")
-    else:
-        print(f"{Fore.RED}[!]{Fore.WHITE} something went wrong: {response.status_code} - {response.text}")
+def changepfp(webhook_url): # taken from https://github.com/infamouskoala/koalahook/blob/main/koalahook.py
+    global successful_commands
+    image_path = input(f"{Fore.BLUE}[?]{Fore.WHITE} Path/URL to image: ").strip()
+    try:
+        if image_path.startswith(('http://', 'https://')):
+            response = requests.get(image_path)
+            response.raise_for_status()
+            encoded_image = base64.b64encode(response.content).decode('utf-8')
+        else:
+            with open(image_path, "rb") as image_file:
+                encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+
+        data = {
+            "avatar": f"data:image/jpeg;base64,{encoded_image}"
+        }
+        response = requests.patch(webhook_url, json=data)
+        response.raise_for_status()
+        successful_commands += 1
+        print(f"{Fore.GREEN}[!]{Fore.WHITE} Profile picture changed successfully.")
+    except Exception as e:
+        print(f"{Fore.RED}[!]{Fore.WHITE} Error: {e}")
+
+def main(webhook_url, delay, amount, message):
+    global successful_commands
+    counter = 0
+    try:
+        while True if amount == "inf" else counter < int(amount):
+            try:
+                data = requests.post(webhook_url, json={"content": str(message)})
+                if data.status_code == 204:
+                    successful_commands += 1
+                    print(f"{Fore.MAGENTA}[+]{Fore.WHITE} Sent '{message}' ({counter} times)")
+                else:
+                    print(f"{Fore.RED}[-]{Fore.WHITE} Failed to send message")
+            except Exception as e:
+                print(f"{Fore.RED}[!] Error: {e}")
+            time.sleep(float(delay))
+            counter += 1
+    except KeyboardInterrupt:
+        print(f"\n{Fore.GREEN}[!]{Fore.WHITE} Spam stopped by user. Total messages sent: {counter}")
 
 def get_webhook_details(webhook_url):
     response = requests.get(webhook_url)
@@ -81,26 +103,25 @@ def get_webhook_details(webhook_url):
         time.sleep(0.1)
     else:
         print(f"    {Fore.RED}[!]{Fore.WHITE} something went wrong: {response.status_code} - {response.text}")
-
-def spam_webhook(webhook_url, message):
-    counter = 0
-    try:
-        while True:
-            response = requests.post(webhook_url, json={"content": message})
-            if response.status_code == 204:
-                counter += 1
-                print(f"{Fore.MAGENTA}[+]{Fore.WHITE} Sent '{message}' ({counter} times)")
-            elif response.status_code == 429:  # Rate limit
-                retry_after = response.json().get("retry_after", 3) / 3000  # Millisekunder til sekunder
-                print(f"{Fore.RED}[!] Rate limited. Waiting {retry_after:.2f} seconds.")
-                time.sleep(retry_after)
-            else:
-                print(f"{Fore.RED}[!] Error: {response.status_code} - {response.text}")
-                break
-    except KeyboardInterrupt:
-        print(f"\n{Fore.GREEN}[!]{Fore.WHITE} Spam stopped by user. Total messages sent: {counter}")
-
-    
+        
+def gui():
+    print(f" ┌──({name}㉿{Fore.MAGENTA}moon{Fore.WHITE})-[~]")
+    print(f" └─{Fore.GREEN}${Fore.WHITE} Webhook commands")
+    print(f" ╔══════════════════════════════════╗")
+    time.sleep(0.1)
+    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} -s. Spam webhook             ║")
+    time.sleep(0.1)
+    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} -d. Delete webhook           ║")
+    time.sleep(0.1)
+    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} -r. Rename the webhook       ║")
+    time.sleep(0.1)
+    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} -p. Changes the webhook pfp  ║")
+    time.sleep(0.1)
+    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} exit. Exits the program      ║")
+    time.sleep(0.1)
+    print(f" ╚══════════════════════════════════╝")
+    time.sleep(0.1)
+    print()
 
 def animated_print(text, delay=0.05):
     for char in text:
@@ -151,6 +172,7 @@ while True:
     print(f"{Fore.WHITE}")
     print(f"                Logged in as {Fore.BLUE}{name}{Fore.WHITE} with {Fore.BLUE}{webhook_name}{Fore.WHITE} as webhook")
     print(f"{Fore.WHITE}")
+    print(f" {Fore.BLUE}[i]{Fore.WHITE} Total successful commands: {successful_commands}")
     print()
     print(f" ┌──({name}㉿{Fore.MAGENTA}moon{Fore.WHITE})-[~]")
     print(f" └─{Fore.GREEN}${Fore.WHITE} Webhook info")
@@ -160,31 +182,24 @@ while True:
     animated_fast(" - - - - - - - - - - - - - - - -")
     print()
     print()
-    print(f" ┌──({name}㉿{Fore.MAGENTA}moon{Fore.WHITE})-[~]")
-    print(f" └─{Fore.GREEN}${Fore.WHITE} Webhook commands")
-    print(f" ╔══════════════════════════════════╗")
-    time.sleep(0.1)
-    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} -s. Spam webhook             ║")
-    time.sleep(0.1)
-    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} -d. Delete webhook           ║")
-    time.sleep(0.1)
-    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} -r. Rename the webhook       ║")
-    time.sleep(0.1)
-    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} -p. Changes the webhook pfp  ║")
-    time.sleep(0.1)
-    print(f" ║ {Fore.BLUE}[!]{Fore.WHITE} exit. Exits the program      ║")
-    time.sleep(0.1)
-    print(f" ╚══════════════════════════════════╝")
-    time.sleep(0.1)
-    print()
+    gui()
     print(f" ┌──({name}㉿{Fore.MAGENTA}moon{Fore.WHITE})-[~]")
     valg2 = input(f" └─{Fore.GREEN}${Fore.WHITE} ").strip()
     print()
     print("")
 
     if valg2 == "-s":
-        message = input(f"{Fore.BLUE}[?]{Fore.WHITE} Message to spam: ").strip()
-        spam_webhook(webhook_url, message)
+        message = input(f"{Fore.BLUE}[?]{Fore.WHITE} Message to spam: ")
+        delay = input(f"{Fore.BLUE}[?]{Fore.WHITE} Enter a delay [int/float] > ")
+        amount = input(f"{Fore.BLUE}[?]{Fore.WHITE} Enter an amount [int/inf] > ")
+        try:
+            delay = float(delay)
+        except ValueError:
+            exit()
+        else:
+            main(webhook_url, delay, amount, message)
+            time.sleep(1)
+            os.system("cls" if os.name == "nt" else "clear")
     elif valg2 == "-d":
         response = requests.delete(webhook_url)
         if response.status_code == 204:
@@ -202,8 +217,7 @@ while True:
         time.sleep(1)
         os.system("cls" if os.name == "nt" else "clear")
     elif valg2 == "-p":
-        new_avatar_url = input(f"{Fore.BLUE}[?]{Fore.WHITE} URL to webhook pfp: ").strip()
-        update_webhook_profile(webhook_url, new_avatar_url)
+        changepfp(webhook_url)
         time.sleep(1)
         os.system("cls" if os.name == "nt" else "clear")
     elif valg2 == "exit":
